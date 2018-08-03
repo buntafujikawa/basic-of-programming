7c3599db4c226f0ca1c6f942025aeac38ea3960d
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-08-03 19:49:05 +0900

    add

--- exercise/8_1.ml
+++ exercise/8_1.ml
@@ -4,3 +4,4 @@ type okozukai_t = { title : string; author : string; publisher : string; price :
 let test1 = { title = "リーダブルコード"; author = "Dustin Boswell"; publisher = "OREILY"; price = 1000; isbn = "978" }
 let test2 = { title = "ノンデザイナーズデザインブック"; author = "Robin Williams"; publisher = "マイナビ"; price = 1000; isbn = "978" }
 let test3 = { title = "プログラミングの基礎"; author = "浅井健一"; publisher = "サイエンス社"; price = 2300; isbn = "978" }
+
