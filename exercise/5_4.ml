936142942e55f1af3bfde7d35c88da56f27af126
buntafujikawa <bunta.fujikawa@gmail.com>
2018-08-03 22:09:44 +0900

    add

--- exercise/5_4.ml
+++ exercise/5_4.ml
@@ -1,6 +1,8 @@
 (* 2次方程式ax2 + bx + c =0の係数a,b,cを与えられたら判別式の値を返す *)
-(* hanbetsushiki : int -> int -> int -> string *)
+(* hanbetsushiki : float -> float -> float -> float *)
+let hanbetsushiki a b c = b ** 2. -. 4. *. a *. c
 
+let test1 = hanbetsushiki 1. 1. 1. = -3.
+let test2 = hanbetsushiki 1. 2. 1. = 0.
+let test3 = hanbetsushiki 1. 4. 1. = 12.
 
-
-let test1 = hanbetsushiki 1 1 1 = ?
