64622a31201d33c45244b58f745223a04cc0136d
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-08-11 16:36:31 +0900

    add

--- exercise/fac.ml
+++ exercise/fac.ml
@@ -9,3 +9,14 @@ let test2 = fac 1 = 1
 let test3 = fac 2 = 2
 let test4 = fac 3 = 6
 let test5 = fac 10 = 3628800
+
+(* 自然数mとnを受け取ったらmのn乗を返す *)
+(* power : int -> int -> int *)
+let rec power m n =
+    if n = 0 then 1
+             else m * power m (n -1)
+
+let test1 = power 3 0 = 1
+let test2 = power 3 1 = 3
+let test3 = power 3 2 = 9
+let test4 = power 3 3 = 27
