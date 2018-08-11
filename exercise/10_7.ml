cd8b31ea9cd99aa71ec4dc2737c78d108ab8e232
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-08-11 12:58:36 +0900

    add

--- exercise/10_7.ml
+++ exercise/10_7.ml
@@ -22,7 +22,22 @@ let rec ketsueki_shukei person_list = match person_list with
 let person1 = { name = "鈴木";  height = 170.0; weight = 64.5; month = 1; day = 2; blood_type = "A" }
 let person2 = { name = "田中";  height = 180.0; weight = 74.9; month = 2; day = 3; blood_type = "B" }
 let person3 = { name = "佐藤";  height = 190.0; weight = 100.0; month = 3; day = 4; blood_type = "A" }
+let person4 = { name = "佐藤";  height = 190.0; weight = 100.0; month = 3; day = 4; blood_type = "B" }
 
 let test1 = ketsueki_shukei [] = (0,0,0,0)
 let test2 = ketsueki_shukei [person1] = (1,0,0,0)
 let test3 = ketsueki_shukei [person1; person2; person3] = (2,1,0,0)
+
+
+(* person_t型のデータのリストを受け取ったら、4つの血液型のうち最も人気の多かった血液型を返す *)
+(* saita_ketsueki : person_t list -> string *)
+let saita_ketsueki person_list =
+    let (a,b,ab,o) = ketsueki_shukei person_list in
+        if a >= b && a >= ab && a >= o then "A"
+        else if b >= a && b >= ab && b >= o then "B"
+        else if ab >= a && ab >= b && ab >= o then "AB"
+        else "O"
+
+let test1 = saita_ketsueki [] = "A"
+let test2 = saita_ketsueki [person1; person2] = "A"
+let test3 = saita_ketsueki [person1; person2; person4] = "B"
