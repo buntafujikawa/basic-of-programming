57d27f62fa6d8a3ccea10a880d1294490d3c24c4
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-08-11 16:53:03 +0900

    add

--- exercise/11_1.ml
+++ exercise/11_1.ml
@@ -7,3 +7,13 @@ let rec sum_of_square n =
 let test1 = sum_of_square 0 = 0
 let test2 = sum_of_square 2 = 5
 let test3 = sum_of_square 4 = 30
+
+(* 漸化式で定義され得る数列のnを求める *)
+(* a : int *)
+let rec a n =
+    if n = 0 then 3
+    else 2 * (a (n -1)) - 1
+
+let test1 = a 0 = 3
+let test2 = a 1 = 5
+let test3 = a 2 = 9
