41f5807eb7113c142486f2a7cc570173c0c84077
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-08-12 12:29:45 +0900

    add

--- exercise/12_1-3.ml
+++ exercise/12_1-3.ml
@@ -23,3 +23,21 @@ let test1 = make_eki_list [
             {namae="代々木公園"; saitan_kyori=infinity; temae_list=[]};
             {namae="明治神宮前"; saitan_kyori=infinity; temae_list=[]};
         ]
+
+(* eki_t型のリストと起点を受け取ったら、起点のみ上記のようになっているeki_t型のリストを返す *)
+(* shokika : eki_t list -> string -> ekit_t list *)
+let rec shokika eki_list kiten = match eki_list with
+    [] -> []
+    | ({namae=namae; saitan_kyori=saitan_kyori; temae_list=temae_list}) as first :: rest ->
+        if namae = kiten then {namae=namae; saitan_kyori=0.; temae_list=[namae]} :: shokika rest kiten
+                         else first :: shokika rest kiten
+
+let test1 = shokika [
+    {namae="代々木上原"; saitan_kyori=infinity; temae_list=[]};
+    {namae="代々木公園"; saitan_kyori=infinity; temae_list=[]};
+    {namae="明治神宮前"; saitan_kyori=infinity; temae_list=[]};
+] "代々木上原" = [
+    {namae="代々木上原"; saitan_kyori=0.; temae_list=["代々木上原"]};
+    {namae="代々木公園"; saitan_kyori=infinity; temae_list=[]};
+    {namae="明治神宮前"; saitan_kyori=infinity; temae_list=[]};
+]
