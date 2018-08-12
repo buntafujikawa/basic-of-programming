af81dcb042de86a7f5ce5acbdddad8c859dffba6
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-08-12 16:20:35 +0900

    add

--- exercise/13_3-5.ml
+++ exercise/13_3-5.ml
@@ -13,3 +13,11 @@ let hoge4 fuga1 f = f fuga1
 (* ('a -> 'b) -> ('b -> 'c) -> 'a -> 'c *)
 let hoge2 f1 f2 x = f2 (f1 x)
 
+let add3 x = x + 3;;
+let times2 x = x * 2;;
+
+(* 関数を2つ受け取ったら、その2つの関数を合成した関数を返す *)
+(* compose : (int -> int) -> (int -> int) -> int -> int *)
+let compose f1 f2 x = f1 (f2 x)
+
+let test1 = compose times2 add3 4 = 14
