24add419ce8682a9825f4d861d84680d9a7e9649
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-08-12 14:44:29 +0900

    add

--- exercise/12_4.ml
+++ exercise/12_4.ml
@@ -7,6 +7,7 @@ let rec insert ekimei_list ekimei = match ekimei_list with
     | ({ kanji=kanji_list; kana=kana_list; romaji=romaji_list; shozoku=shozoku_list; }) as first :: rest ->
     let { kanji=kanji; kana=kana; romaji=romaji; shozoku=shozoku; } = ekimei in
         if kana < kana_list then ekimei :: ekimei_list
+        else if kana = kana_list then insert rest ekimei
         else first :: insert rest ekimei
 
 let test1 = insert [
@@ -31,14 +32,24 @@ let test2 = insert [
     {kanji="代々木公園"; kana="よよぎこうえん"; romaji="yoyogikouen"; shozoku="千代田線"};
 ]
 
-(* ekimei_tのリストを受け取ったら、それを昇順に整列したリストを返す *)
-(* ekimei_t list -> ekimei_t list *)
-let rec ins_sort ekimei_list = match ekimei_list with
-    [] -> []
-    | first :: rest -> insert (ins_sort rest) first
+let test3 = insert [
+    {kanji="表参道"; kana="おもてさんどう"; romaji="omotesandou"; shozoku="千代田線"};
+    {kanji="明治神宮前"; kana="めいじじんぐうまえ"; romaji="meijijinguumae"; shozoku="千代田線"};
+    {kanji="代々木上原"; kana="よよぎうえはら"; romaji="yoyogiuehara"; shozoku="千代田線"};
+    ] {kanji="代々木上原"; kana="よよぎうえはら"; romaji="yoyogiuehara"; shozoku="千代田線"} = [
+    {kanji="表参道"; kana="おもてさんどう"; romaji="omotesandou"; shozoku="千代田線"};
+    {kanji="明治神宮前"; kana="めいじじんぐうまえ"; romaji="meijijinguumae"; shozoku="千代田線"};
+    {kanji="代々木上原"; kana="よよぎうえはら"; romaji="yoyogiuehara"; shozoku="千代田線"};
+]
 
-let test1 = ins_sort [] = []
-let test2 = ins_sort [
+(* ekimei_t型のリストを受け取ったら、それをひらがなの順に整列し、さらに駅の重複を取り除いたekimei_t型のリストを返す *)
+(* seiretsu : ekimei_t list -> ekimei_t list *)
+let rec seiretsu ekimei_list = match ekimei_list with
+    [] -> []
+    | first :: rest -> insert (seiretsu rest) first
+
+let test1 = seiretsu [] = []
+let test2 = seiretsu [
     {kanji="代々木公園"; kana="よよぎこうえん"; romaji="yoyogikouen"; shozoku="千代田線"};
     {kanji="明治神宮前"; kana="めいじじんぐうまえ"; romaji="meijijinguumae"; shozoku="千代田線"};
     {kanji="表参道"; kana="おもてさんどう"; romaji="omotesandou"; shozoku="千代田線"};
@@ -46,6 +57,15 @@ let test2 = ins_sort [
 ] = [
     {kanji="表参道"; kana="おもてさんどう"; romaji="omotesandou"; shozoku="千代田線"};
     {kanji="明治神宮前"; kana="めいじじんぐうまえ"; romaji="meijijinguumae"; shozoku="千代田線"};
+    {kanji="代々木上原"; kana="よよぎうえはら"; romaji="yoyogiuehara"; shozoku="千代田線"};
+    {kanji="代々木公園"; kana="よよぎこうえん"; romaji="yoyogikouen"; shozoku="千代田線"};
+]
+let test3 = seiretsu [
+    {kanji="代々木公園"; kana="よよぎこうえん"; romaji="yoyogikouen"; shozoku="千代田線"};
+    {kanji="代々木公園"; kana="よよぎこうえん"; romaji="yoyogikouen"; shozoku="千代田線"};
+    {kanji="代々木公園"; kana="よよぎこうえん"; romaji="yoyogikouen"; shozoku="千代田線"};
+    {kanji="代々木上原"; kana="よよぎうえはら"; romaji="yoyogiuehara"; shozoku="千代田線"};
+] = [
     {kanji="代々木上原"; kana="よよぎうえはら"; romaji="yoyogiuehara"; shozoku="千代田線"};
     {kanji="代々木公園"; kana="よよぎこうえん"; romaji="yoyogikouen"; shozoku="千代田線"};
 ]
