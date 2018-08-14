19ee9a0b796c8f6f0b7f667102df1a43ed272fd1
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-08-14 22:14:58 +0900

    add

--- exercise/14_3.ml
+++ exercise/14_3.ml
@@ -1,5 +1,7 @@
-(* initから初めてlstの要素を右から順にfを施し込む *)
-(* fold_right : ('a -> 'b -> 'b) -> 'a list -> 'b -> 'b *)
-let rec fold_right f lst init = match lst with
-    [] -> init
-    | first :: rest -> f first (fold_right f rest init)
+(* 文字列のリストを受け取ったら、その中の要素を前から順に全部くっつけた文字列を返す *)
+(* concat : string list -> string *)
+let concat_two_string first second = first ^ second
+let concat list = List.fold_right concat_two_string list ""
+
+let test1 = concat [] = ""
+let test2 = concat ["春"; "夏"; "秋"; "冬"] = "春夏秋冬"
