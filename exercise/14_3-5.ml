ed24221d7675e079c896fe4a0cc698398475e542
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-08-15 22:43:00 +0900

    add

--- exercise/14_3-5.ml
+++ exercise/14_3-5.ml
@@ -9,7 +9,9 @@ let rec fold_right f lst init = match lst with
 (* 文字列のリストを受け取ったら、その中の要素を前から順に全部くっつけた文字列を返す *)
 (* concat : string list -> string *)
 let concat_two_string first second = first ^ second
-let concat list = List.fold_right concat_two_string list ""
+let concat list =
+    let concat_two_string first second = first ^ second in
+    List.fold_right concat_two_string list ""
 
 let test1 = concat [] = ""
 let test2 = concat ["春"; "夏"; "秋"; "冬"] = "春夏秋冬"
