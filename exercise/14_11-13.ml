52a04ba27ce6af6f1b5ee000680026ce53e70617
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-08-16 12:51:35 +0900

    add

--- exercise/14_11-13.ml
+++ exercise/14_11-13.ml
@@ -37,3 +37,23 @@ let test1 = shokika [
     {namae="代々木公園"; saitan_kyori=infinity; temae_list=[]};
     {namae="明治神宮前"; saitan_kyori=infinity; temae_list=[]};
 ]
+
+(* make_eki_listとshokikaを一度にやってしまう *)
+(* make_initial_eki_list : ekimei_t list -> strign -> eki_t list *)
+let make_initial_eki_list ekimei_list kiten =
+    List.map (fun ekimei -> match ekimei with
+                | { kanji=kanji; kana=kana; romaji=romaji; shozoku=shozoku; }
+                -> if kanji = kiten then {namae=kanji; saitan_kyori=0.; temae_list=[kanji]}
+                                    else {namae=kanji; saitan_kyori=infinity; temae_list=[]}
+                ) ekimei_list
+
+let test1 = make_initial_eki_list [
+        {kanji="代々木上原"; kana="よよぎうえはら"; romaji="yoyogiuehara"; shozoku="千代田線"};
+        {kanji="代々木公園"; kana="よよぎこうえん"; romaji="yoyogikouen"; shozoku="千代田線"};
+        {kanji="明治神宮前"; kana="めいじじんぐうまえ"; romaji="meijijinguumae"; shozoku="千代田線"};
+        ] "代々木上原" =
+        [
+            {namae="代々木上原"; saitan_kyori=0.; temae_list=["代々木上原"]};
+            {namae="代々木公園"; saitan_kyori=infinity; temae_list=[]};
+            {namae="明治神宮前"; saitan_kyori=infinity; temae_list=[]};
+        ]
