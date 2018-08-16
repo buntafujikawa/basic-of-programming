e77876874b01eb3e46b5bf7b2ca7713473c6943a
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-08-16 14:55:45 +0900

    add

--- exercise/15_1.ml
+++ exercise/15_1.ml
@@ -1,14 +1,8 @@
 (* 受け取ったlstをクイックソートを使って昇順に整列する *)
 (* quick_sort : int list -> int list *)
 let rec quick_sort lst =
-    (* lstの中からnよりpである要素のみを取り出す *)
-    (* take : int -> int list -> (int -> int -> bool) -> int list *)
     let take n lst p = List.filter (fun item -> p item n) lst
-    (* lstの中からnより小さい要素のみを取り出す *)
-    (* take_less : int -> int list -> int list *)
-    in let take_less n lst = take n lst (<)
-    (* lstの中からnより大きい要素のみを取り出す *)
-    (* take_greater : int -> int list -> int list *)
+    in let take_less n lst = take n lst (<=)
     in let take_greater n lst = take n lst (>)
     in match lst with
     [] -> [] (* 自明に答えが出るケース *)
@@ -21,4 +15,5 @@ let test2 = quick_sort [1] = [1]
 let test3 = quick_sort [1; 2] = [1; 2]
 let test4 = quick_sort [2; 1] = [1; 2]
 let test5 = quick_sort [5; 4; 9; 8; 2; 3] = [2; 3; 4; 5; 8; 9]
-let test6 = quick_sort [1; 1; 2] != [1; 1; 2]
+let test6 = quick_sort [1; 1; 2] = [1; 1; 2]
+let test6 = quick_sort [2; 1; 1; 2] = [1; 1; 2; 2]
