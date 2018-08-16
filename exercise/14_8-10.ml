a01b578926d0c3d5067e841e5dfa153c63f55fe8
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-08-16 12:04:07 +0900

    add

--- exercise/14_8-10.ml
+++ exercise/14_8-10.ml
@@ -1,5 +1,14 @@
+#use "10_7-8.ml"
+
 (* 整数を受け取ってきたら、その2乗から1を引いた数を返す *)
 let square_minus_one = fun num -> num * num - 1
 
 let test1 = square_minus_one 1 = 0
 let test2 = square_minus_one 3 = 8
+
+(* person_t型のデータを受け取ってきたら、その名前フィールドを取り出す *)
+let get_name = fun person -> match person with
+    {name=name;height=height;weight=weight;month=month;day=day;blood_type=blood_type;} -> name
+
+let test1 = get_name person1 = "鈴木"
+let test2 = get_name person2 = "田中"
