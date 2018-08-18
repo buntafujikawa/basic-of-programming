da26b57aa74ced8fd65aab21bcc7d287fca213ab
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-08-18 22:24:38 +0900

    add

--- exercise/16_3-4.ml
+++ exercise/16_3-4.ml
@@ -1,4 +1,5 @@
 #use "10_10-11.ml"
+(*#use "15_4.ml"*)
 
 (* 直前に確定した駅pと未確定の駅のリストvを受け取ったら、必要な更新処理を行った後の未確定の駅のリストを返す(14.13を駅間のリストを引数で渡せるようにする) *)
 (* koushin : eki_t -> eki_t list -> ekikan_t list -> eki_t list *)
@@ -33,3 +34,27 @@ let test2 = koushin
         {namae="明治神宮前"; saitan_kyori=2.2; temae_list=["明治神宮前";"代々木公園"; "代々木上原"];};
         {namae="表参道"; saitan_kyori=infinity; temae_list=[];};
     ]
+
+(* 駅のリストと駅間のリストを受け取ったら、ダイクストラのアルゴリズムに従って各駅について最短距離と最短経路が正しく入ったリストを返す *)
+(* dijkstra_main : eki_t list -> ekikan_t list -> eki_t list *)
+let rec dijkstra_main eki_list ekikan_list = match eki_list with
+    [] -> []
+    | first :: rest ->
+        let (saitan, others) = saitan_wo_bunri (first :: rest)
+        in let mikakutei_eki_list = koushin saitan others ekikan_list
+        in saitan :: dijkstra_main mikakutei_eki_list ekikan_list
+
+let eki_list = [
+    {namae="池袋"; saitan_kyori = infinity; temae_list = []};
+    {namae="新大塚"; saitan_kyori = 1.2; temae_list = ["新大塚"; "茗荷谷"]};
+    {namae="茗荷谷"; saitan_kyori = 0.; temae_list = ["茗荷谷"]};
+    {namae="後楽園"; saitan_kyori = infinity; temae_list = []};
+]
+
+let test1 = dijkstra_main [] global_ekikan_list = []
+let test2 = dijkstra_main eki_list global_ekikan_list =[
+    {namae = "茗荷谷"; saitan_kyori = 0.; temae_list = ["茗荷谷"]};
+    {namae = "新大塚"; saitan_kyori = 1.2; temae_list = ["新大塚"; "茗荷谷"]};
+    {namae = "後楽園"; saitan_kyori = 1.8; temae_list = ["後楽園"; "茗荷谷"]};
+    {namae = "池袋"; saitan_kyori = 3.; temae_list = ["池袋"; "新大塚"; "茗荷谷"]}
+]
