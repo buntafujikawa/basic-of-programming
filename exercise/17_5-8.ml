75f42692d50d2ca87d78a5fe75779131cb6b25da
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-08-22 11:50:23 +0900

    add

--- exercise/17_5-8.ml
+++ exercise/17_5-8.ml
@@ -14,6 +14,7 @@ let tree1 = Empty
 let tree2 = Leaf (3)
 let tree3 = Node (tree1, 4, tree2)
 let tree4 = Node (tree2, 5, tree3)
+let tree5 = Node (tree3, 5, tree3)
 
 let test1 = sum_tree tree1 = 0
 let test2 = sum_tree tree2 = 3
@@ -55,3 +56,16 @@ let test1 = tree_length tree1 = 0
 let test2 = tree_length tree2 = 1
 let test3 = tree_length tree3 = 2
 let test4 = tree_length tree4 = 4
+
+(* 木の深さを返す *)
+(* tree_depth: tree_t -> int *)
+let rec tree_depth tree = match tree with
+    Empty -> 0
+  | Leaf (n) -> 0
+  | Node (t1, n, t2) -> 1 + max (tree_depth t1) (tree_depth t2)
+
+let test1 = tree_depth tree1 = 0
+let test2 = tree_depth tree2 = 0
+let test3 = tree_depth tree3 = 1
+let test4 = tree_depth tree4 = 2
+let test5 = tree_depth tree5 = 2
