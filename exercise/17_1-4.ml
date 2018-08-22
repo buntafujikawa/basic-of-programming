dbe4d6e9835900f2d3379cd008852a13026d3159
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-08-22 10:20:55 +0900

    add

--- exercise/17_1-4.ml
+++ exercise/17_1-4.ml
@@ -47,3 +47,22 @@ type seiza_t = Capricorn
                | Libra
                | Scorpio
                | Sagittarius
+
+(* year_t型を受け取ってきたら、seiza_tがたの星座を返す *)
+(* seiza : year_t -> seiza_t *)
+let seiza year = match year with
+    January (n) -> if n <= 19 then Capricorn else Aquarius
+  | February (n) -> if n <= 18 then Aquarius else Pisces
+  | March (n) -> if n <= 20 then Pisces else Aries
+  | April (n) -> if n <= 19 then Aries else Taurus
+  | May (n) -> if n <= 20 then Taurus else Gemini
+  | June (n) -> if n <= 21 then Gemini else Cancer
+  | July (n) -> if n <= 22 then Cancer else Leo
+  | August (n) -> if n <= 22 then Leo else Virgo
+  | September (n) -> if n <= 22 then Virgo else Libra
+  | October (n) -> if n <= 23 then Libra else Scorpio
+  | November (n) -> if n <= 22 then Scorpio else Sagittarius
+  | December (n) -> if n <= 23 then Sagittarius else Capricorn
+
+let test1 = seiza (January (1)) = Capricorn
+let test1 = seiza (March (25)) = Aries
