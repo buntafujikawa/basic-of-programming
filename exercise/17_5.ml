77ddaa4149dc39dff7001af2d83ed1601d26a1f8
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-08-22 11:30:56 +0900

    add

--- exercise/17_5.ml
+++ exercise/17_5.ml
@@ -30,3 +30,16 @@ let test1 = tree_double tree1 = Empty
 let test2 = tree_double tree2 = Leaf (6)
 let test3 = tree_double tree3 = Node (Empty, 8, (Leaf (6)))
 let test4 = tree_double tree4 = Node (Leaf (6), 10, Node (Empty, 8, Leaf (6)))
+
+(* 節や葉に入っている値全てにfを適用した木を返す *)
+(* tree_map : (int -> int) -> tree_t -> tree_t *)
+let rec tree_map f tree = match tree with
+    Empty -> Empty
+  | Leaf (n) -> Leaf (f n)
+  | Node (t1, n, t2) -> Node (tree_map f t1, f n, tree_map f t2)
+
+let f n = n * 2
+let test1 = tree_map f tree1 = Empty
+let test2 = tree_map f tree2 = Leaf (6)
+let test3 = tree_map f tree3 = Node (Empty, 8, (Leaf (6)))
+let test4 = tree_map f tree4 = Node (Leaf (6), 10, Node (Empty, 8, Leaf (6)))
