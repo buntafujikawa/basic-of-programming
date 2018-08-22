fe9ac68b66e0dd89b9fc48fc35e1896fc2f69866
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-08-22 16:38:59 +0900

    add

--- exercise/17_10-13.ml
+++ exercise/17_10-13.ml
@@ -43,3 +43,17 @@ let test2 = tree2 =
     Node (Empty, "新大塚", [("茗荷谷", 1.2); ("池袋", 1.8)],
     Node (Empty, "池袋", [("新大塚", 1.8)],
     Node (Empty, "茗荷谷", [("新大塚", 1.2)], Empty)))
+
+(* リストの中に含まれる駅間を全て挿入した木を返す *)
+(* inserts_ekikan : ekikan_tree_t -> ekikan_t list -> ekikan_t list *)
+let rec inserts_ekikan ekikan_tree ekikan_list =
+    List.fold_left insert_ekikan ekikan_tree ekikan_list
+
+let test1 = inserts_ekikan Empty [ekikan1; ekikan2; ekikan3] =
+    Node (Node (Empty, "後楽園", [("茗荷谷", 1.8)], Empty),
+    "新大塚", [("茗荷谷", 1.2); ("池袋", 1.8)],
+        Node (Empty,
+        "池袋", [("新大塚", 1.8)],
+        Node (Empty,
+        "茗荷谷", [("後楽園", 1.8); ("新大塚", 1.2)],
+        Empty)))
